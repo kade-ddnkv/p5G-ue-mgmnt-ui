4061c272752f20daefab70435ef76cb904e6c962
kade-ddnkv <88973774+kade-ddnkv@users.noreply.github.com>
2023-12-20 16:45:34 +0300

    try 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon, PlusIcon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon, PlusIcon, TrashIcon, PencilIcon, DocumentTextIcon, DocumentIcon } from '@heroicons/react/24/outline'
 
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main>
-      <div id='add-ue' style={{ display: "none" }}>
+      <div id='add-ue' style={{ display: "none" }} className='relative z-10'>
         <div className='bg-gray-800 bg-opacity-50 h-screen w-screen absolute'></div>
         <div onClick={() => {
           var elem = document.getElementById('add-ue')
@@ -16,14 +16,26 @@ export default function Home() {
             elem.style.display = "none";
           }
         }} className="flex h-screen w-full justify-center items-center absolute">
-          <div className="2xl:w-5/12 2xl:h-4/5 bg-white rounded-md">
-            <div className='px-8 py-4'>
+          <div onClick={(event) => {
+            event.stopPropagation()
+          }} className="xl:w-5/12 xl:h-4/5 bg-white rounded-md">
+            <div className='px-10 py-6'>
               <p className='text-lg font-bold'>Add new UE manually</p>
+              <div className="grid gap-x-1 grid-cols-4 grid-rows-1 pt-2 items-center w-8/12 mt-4">
+                <p className='mb-1'>IMSI</p>
+                <input className="mb-1 col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="208930000000001"></input>
+              </div>
+              <div className="grid gap-y-2 gap-x-1 grid-cols-4 grid-rows-2 items-center w-8/12 mt-3">
+                <p>K</p>
+                <input className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="548327912488348294384"></input>
+                <p>OPC</p>
+                <input className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="548327912488348294384"></input>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex min-h-screen justify-center">
+      <div className="flex min-h-screen justify-center relative">
         <div className="2xl:w-6/12 2xl:h-screen bg-white overflow-clip relative">
           <div className="px-20 py-10">
             <p className="font-bold text-xl">User Equipment manager</p>
@@ -36,19 +48,32 @@ export default function Home() {
                 </Menu.Button>
               </div>
             </Menu> */}
-              <div onClick={() => {
-                var elem = document.getElementById('add-ue')
-                if (elem != null) {
-                  elem.style.display = "block";
-                }
-              }} className='cursor-pointer hover:bg-gray-200 rounded-full h-12 w-12 bg-gray-100 flex items-center justify-center'><PlusIcon className='h-5 w-5'></PlusIcon></div>
-              <div className='rounded-full bg-gray-100 absolute top-10'
-                style={{ height: "50rem", width: "50rem", transform: "translateY(-20rem) translateX(20rem)" }}>
-                <div style={{ transform: "translateY(25rem)" }}>
-                  <p className='font-semibold text-xl' style={{ transform: "translateX(5rem)" }}>Add new UE</p>
-                  <p className='text-lg' style={{ transform: "translateY(5rem) translateX(7rem)" }}>Add manually</p>
-                  <p className='text-lg' style={{ transform: "translateY(8rem) translateX(10rem)" }}>Upload .csv list</p>
-                  <p className='text-lg' style={{ transform: "translateY(11rem) translateX(15rem)" }}>Upload backup file</p>
+              <div className='flex flex-row-reverse justify-start items-center'>
+                <div onClick={() => {
+                  var elem = document.getElementById('add-ue')
+                  if (elem != null) {
+                    elem.style.display = "block";
+                  }
+                }}
+                  id='add-button'
+                  className='cursor-pointer hover:bg-gray-200 rounded-full h-12 w-12 bg-gray-100 flex items-center justify-center'>
+                  <PlusIcon className='h-5 w-5'></PlusIcon>
+                </div>
+                <div className='invisible hidden-button flex flex-row-reverse justify-start items-center'>
+                  <div onClick={() => {
+                    var elem = document.getElementById('add-ue')
+                    if (elem != null) {
+                      elem.style.display = "block";
+                    }
+                  }} className='cursor-pointer rounded-full h-10 w-10 bg-gray-100 mr-6 flex items-center justify-center'>
+                    <PencilIcon className='h-5 w-5'></PencilIcon>
+                  </div>
+                  <div className='cursor-pointer rounded-full h-10 w-10 bg-gray-100 mr-4 flex items-center justify-center'>
+                    <DocumentTextIcon className='h-5 w-5'></DocumentTextIcon>
+                  </div>
+                  <div className='cursor-pointer rounded-full h-10 w-10 bg-gray-100 mr-4 flex items-center justify-center'>
+                    <DocumentIcon className='h-5 w-5'></DocumentIcon>
+                  </div>
                 </div>
               </div>
             </div>
@@ -71,6 +96,12 @@ export default function Home() {
                       <p className='inline'>403 MiB</p>
                     </div>
                   </td>
+                  <td>
+                    <div className='flex flex-row space-x-5'>
+                      <PencilIcon className='cursor-pointer h-5 w-5 text-gray-600'></PencilIcon>
+                      <TrashIcon className='cursor-pointer h-5 w-5 text-gray-600'></TrashIcon>
+                    </div>
+                  </td>
                 </tr>
                 <tr>
                   <td>Western cowhouse</td>
@@ -80,6 +111,12 @@ export default function Home() {
                     <div className='flex flex-row items-center space-x-3'>
                       <div className="h-2 w-16 bg-gray-100"></div>
                       <p className='inline'>934 KiB</p>
+                    </div>
+                  </td>
+                  <td>
+                    <div className='flex flex-row space-x-5'>
+                      <PencilIcon className='cursor-pointer h-5 w-5 text-gray-600'></PencilIcon>
+                      <TrashIcon className='cursor-pointer h-5 w-5 text-gray-600'></TrashIcon>
                     </div>
                   </td>
                 </tr>
@@ -93,6 +130,12 @@ export default function Home() {
                       <p className='inline'>20 MiB</p>
                     </div>
                   </td>
+                  <td>
+                    <div className='flex flex-row space-x-5'>
+                      <PencilIcon className='cursor-pointer h-5 w-5 text-gray-600'></PencilIcon>
+                      <TrashIcon className='cursor-pointer h-5 w-5 text-gray-600'></TrashIcon>
+                    </div>
+                  </td>
                 </tr>
                 {
                   [...Array(5)].map((e, i) => <tr>
@@ -103,6 +146,12 @@ export default function Home() {
                       <div className='flex flex-row items-center space-x-3'>
                         <div className="h-2 w-16 bg-white outline outline-1 outline-gray-100"></div>
                         <p className='inline'>0</p>
+                      </div>
+                    </td>
+                    <td>
+                      <div className='flex flex-row space-x-5'>
+                        <PencilIcon className='cursor-pointer h-5 w-5 text-gray-600'></PencilIcon>
+                        <TrashIcon className='cursor-pointer h-5 w-5 text-gray-600'></TrashIcon>
                       </div>
                     </td>
                   </tr>)
